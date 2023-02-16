696198d7d42c1f6e6d6d9ba72c0cac89a5d819de
hanseu9839 <hanseu9839@gmail.com>
2023-02-16 19:58:41 +0900

    oddSetting

---
+++ src/main/java/com/odd/oddProject/OddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.odd.oddProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odd/oddProject/OddProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odd.oddProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
