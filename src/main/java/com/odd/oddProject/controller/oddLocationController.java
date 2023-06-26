4237547893adc2d9d9bb036b2c70a3111622a66c
hanseu9839 <hanseu9839@gmail.com>
2023-06-26 23:06:53 +0900

    검색어 구현기능 완료

--- src/main/java/com/odd/oddProject/controller/oddLocationController.java
+++ src/main/java/com/odd/oddProject/controller/oddLocationController.java
@@ -1,8 +1,14 @@
 package com.odd.oddProject.controller;
 
+import com.odd.oddProject.batch.BatchOddLocation;
 import com.odd.oddProject.dto.LocationDto;
 import com.odd.oddProject.dto.oddSrchFilterDto;
+import com.odd.oddProject.service.BatchOddServiceImpl;
 import com.odd.oddProject.service.OddLocationService;
+import com.odd.oddProject.service.OddLocationServiceImpl;
+import com.odd.oddProject.service.OpenApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +19,21 @@ import java.util.List;
 @Controller
 @RequestMapping("/odds")
 public class oddLocationController {
-    @Autowired
+
     OddLocationService oddLocationService;
 
-    @GetMapping("/searchLocation")
-    public ModelAndView findLocation(@RequestParam(required = false "keyword") String keyword, ModelAndView mav){
-        List<LocationDto> list = oddLocationService.oddFindLocation(keyword);
+    @Autowired
+    oddLocationController(OddLocationServiceImpl oddLocationService){
+        this.oddLocationService = oddLocationService;
+    }
+    private static Logger LOGGER = LoggerFactory.getLogger(OddLocationService.class);
+    @GetMapping("/main")
+    public ModelAndView findLocation(@RequestParam(required = false, name="keyword") String keyword, ModelAndView mav){
+        List<LocationDto> list= oddLocationService.oddFindLocation(keyword);
+        LOGGER.info("locationDto" + list);
         mav.addObject("list", "Hello, World!");
         mav.setViewName("/odd/main");
         return mav;
     }
+
 }
